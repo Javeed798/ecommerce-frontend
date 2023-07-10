6ba45132ad52b0e9b9a26e88e17356c54e6daef3
D Javeed Sharif <karishmavahida@gmail.com>
2023-07-10 09:00:29 +0530

    Add To Cart

--- src/app/_services/product.service.ts
+++ src/app/_services/product.service.ts
@@ -45,4 +45,8 @@ export class ProductService {
   public placeOrder(orderDetails: OrderDetails) {
     return this.http.post('http://localhost:9090/placeOrder', orderDetails);
   }
+
+  public addToCart(productId: any) {
+    return this.http.get('http://localhost:9090/addToCart/' + productId);
+  }
 }
--- src/app/product-view-details/product-view-details.component.ts
+++ src/app/product-view-details/product-view-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../_model/product.model';
+import { ProductService } from '../_services/product.service';
 
 @Component({
   selector: 'app-product-view-details',
@@ -12,7 +13,11 @@ export class ProductViewDetailsComponent implements OnInit {
 
   selectedProductIndex = 0;
 
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private service: ProductService
+  ) {}
   ngOnInit(): void {
     this.product = this.activatedRoute.snapshot.data['product'];
     console.log(this.product);
@@ -30,5 +35,16 @@ export class ProductViewDetailsComponent implements OnInit {
         id: productId,
       },
     ]);
+  }
+
+  addToCart(productId: any) {
+    this.service.addToCart(productId).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 }
